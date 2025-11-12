1eb46b196368fcf0bb9660987afeb7c7e2852d50
shamiljafarov <shamil.jafarli1@gmail.com>
2025-11-13 03:26:03 +0400

    how it works created

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Login from './Pages/Login/Login'
 import MainPage from './Pages/Main/MainPage'
+import HowWorks from './Components/HowItWorks/HowItWorks'
 
 function App() {
   return (
     <>
       <Routes>
-            <Route path='/' element={<MainPage/>} />
-            <Route path='/login' element={<Login />} />
+        <Route path='/' element={<MainPage />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/' element={<HowWorks />} />
       </Routes>
     </>)
 }
