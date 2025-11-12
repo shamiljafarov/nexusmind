5065dd5d4414b47e795c9e79f0013dea7bacffd4
Sevinc Abbasova <abbasovasevinc889@gmail.com>
2025-11-12 16:29:22 +0400

    partner-section

---
+++ src/Components/Partners-Section/Partners-section.jsx
@@ -0,0 +1,38 @@
+import './Partners-section.scss'
+import sehiyyelogo from '../../assets/images/Logo_of_the_Ministry_of_Healthcare_of_Azerbaijan.png'
+import tebiblogo from '../../assets/images/TƏBİB_logo_PNG.png'
+
+function Partners() {
+    return(
+        <section>
+            <div className="header">
+                <p className='first'>Partnyorlarımızı</p>
+                <p>tanıyın</p>
+            </div>
+            <div className='partners-logo'>
+                <div className='slider'>
+                    <img src={sehiyyelogo} alt='sehiyye-nazirliyi-logo' className='sehiyye-logo'/>
+                    <img src={tebiblogo} alt='tebib-logo' className='tebib-logo'/>
+                    <img src={sehiyyelogo} alt='sehiyye-nazirliyi-logo' className='sehiyye-logo'/>
+                    <img src={tebiblogo} alt='tebib-logo' className='tebib-logo'/>
+                    <img src={sehiyyelogo} alt='sehiyye-nazirliyi-logo' className='sehiyye-logo'/>
+                    <img src={tebiblogo} alt='tebib-logo' className='tebib-logo'/>
+                    <img src={sehiyyelogo} alt='sehiyye-nazirliyi-logo' className='sehiyye-logo'/>
+                    <img src={tebiblogo} alt='tebib-logo' className='tebib-logo'/>
+
+                    <img src={sehiyyelogo} alt='sehiyye-nazirliyi-logo' className='sehiyye-logo'/>
+                    <img src={tebiblogo} alt='tebib-logo' className='tebib-logo'/>
+                    <img src={sehiyyelogo} alt='sehiyye-nazirliyi-logo' className='sehiyye-logo'/>
+                    <img src={tebiblogo} alt='tebib-logo' className='tebib-logo'/>
+                    <img src={sehiyyelogo} alt='sehiyye-nazirliyi-logo' className='sehiyye-logo'/>
+                    <img src={tebiblogo} alt='tebib-logo' className='tebib-logo'/>
+                    <img src={sehiyyelogo} alt='sehiyye-nazirliyi-logo' className='sehiyye-logo'/>
+                    <img src={tebiblogo} alt='tebib-logo' className='tebib-logo'/>
+
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Partners;
--- src/Pages/Main/MainPage.jsx
+++ src/Pages/Main/MainPage.jsx
@@ -1,11 +1,13 @@
 import Header from '../../Components/Header/Header';
 import Hero from '../../Components/Hero/Hero';
+import Partners from '../../Components/Partners-Section/Partners-section';
 
 function MainPage() {
     return(
         <section>
             <Header/>
             <Hero/>
+            <Partners/>
         </section>
     )
 }
